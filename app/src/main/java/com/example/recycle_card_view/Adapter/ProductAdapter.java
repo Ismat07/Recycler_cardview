28409fdd2fe5779ae98534d574a751ee2b2fbc11
ismat <khan.ismat711@gmail.com>
2019-12-21 00:47:00 +0530

    Recycler CardView

---
+++ app/src/main/java/com/example/recycle_card_view/Adapter/ProductAdapter.java
@@ -0,0 +1,67 @@
+package com.example.recycle_card_view.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recycle_card_view.Model.Product;
+import com.example.recycle_card_view.R;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private Context mCtx;
+    private List<Product> productList;
+
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_products, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewShortDesc.setText(product.getShortDesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewTitle,textViewShortDesc,textViewRating,textViewPrice;
+        ImageView imageView;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recycle_card_view/Model/Product.java
@@ -0,0 +1,44 @@
+package com.example.recycle_card_view.Model;
+
+public class Product {
+
+    private int id;
+    private String title;
+    private String shortDesc;
+    private double rating;
+    private double price;
+    private int image;
+
+    public Product(int id, String title, String shortDesc, double rating, double price, int image) {
+        this.id = id;
+        this.title = title;
+        this.shortDesc = shortDesc;
+        this.rating = rating;
+        this.price = price;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
